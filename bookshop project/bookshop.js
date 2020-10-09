15b1ee13a47f080296aef33e9f4baec23cb90626
alialig98 <gardneralicia98@gmail.com>
2020-10-09 21:04:08 +0100

    Alicia bookshop work

---
+++ bookshop project/bookshop.js
@@ -0,0 +1,5 @@
+
+var bookOne = {name:"The Very Hungry Caterpillar one" , author:"Eric Carle one", price: "£5.00", description: "childrens book", image: "https://images-na.ssl-images-amazon.com/images/I/91T7dbZc6LL.jpg"}
+
+var bookTwo = {name:"The Very Hungry Caterpillar two" , author:"Eric Carle two", price: "£5.00", description: "childrens book", image: "https://images-na.ssl-images-amazon.com/images/I/91T7dbZc6LL.jpg"}
+var books =[bookOne, bookTwo]
